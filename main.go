fdbc870713bd6a2121df176b366a2d5dc6733c7a
vincent-peugnet <vincent-peugnet@riseup.net>
2025-07-18 18:42:02 +0200

    add status sub command

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"os"
 	"path/filepath"
 	"slices"
+	"strings"
 	"time"
 
 	"github.com/vincent-peugnet/wsync/api"
@@ -394,6 +395,42 @@ func list() {
 	fmt.Println(selectedids)
 }
 
+func Status() {
+	files, err := os.ReadDir(repoPath)
+	if err != nil {
+		log.Fatalln("could not read folder:", err)
+	}
+
+	database := LoadDatabase()
+
+	var untrackedFiles []string
+	var trackedFiles []string
+	var trackedModifiedFiles []string
+	for _, file := range files {
+		if !file.IsDir() && filepath.Ext(file.Name()) == ".md" {
+			id := strings.TrimSuffix(file.Name(), ".md")
+			_, exist := database.Pages[id]
+			if !exist {
+				untrackedFiles = append(untrackedFiles, id)
+			} else {
+				trackedFiles = append(trackedFiles, id)
+				modified, err := database.HasBeenModified(id)
+				if err != nil {
+					log.Fatalln("error:", err)
+				}
+				if modified {
+					trackedModifiedFiles = append(trackedModifiedFiles, id)
+				}
+			}
+		}
+	}
+	fmt.Println("📦️ Repo contains:")
+	fmt.Println(len(trackedFiles), "tracked file(s)", trackedFiles)
+	fmt.Println("  ↳ including", len(trackedModifiedFiles), "localy edited file(s)", trackedModifiedFiles)
+	fmt.Println(len(untrackedFiles), "untracked file(s)", untrackedFiles)
+
+}
+
 func menu() {
 	var action string
 	form := huh.NewForm(
@@ -443,6 +480,8 @@ func main() {
 			Push()
 		case "list":
 			list()
+		case "status":
+			Status()
 		default:
 			log.Fatalln("invalid sub command")
 		}
